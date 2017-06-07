0fe5d42b0a361c12d1e83dfdc1611c0d2e1a1585
Logikable <spamseanluchen@gmail.com>
2017-06-07 11:26:00 -0400

    does the knight display?

---
+++ dnd/app.js
@@ -0,0 +1,5 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Knight = require('./knight')
+
+ReactDOM.render(<Knight />, document.getElementById('root'))
---
+++ dnd/knight.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var createReactClass = require('create-react-class');
+
+var Knight = createReactClass({
+	render: function() {
+		return <span>♘</span>;
+	}
+});
+
+module.exports = Knight;
