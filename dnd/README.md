31cd851aa41b5ed76744425b78ea45c825c602f7
Logikable <spamseanluchen@gmail.com>
2017-06-07 15:39:39 -0400

    working click-to-move chessboard

---
+++ dnd/README.md
@@ -0,0 +1,1 @@
+# Drag and Drop using React.js
--- dnd/src/app/knight.js
+++ dnd/src/app/knight.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default class Knight extends Component {
-  render() {
-    return <span>♘</span>;
-  }
+	render() {
+		return <span style={{ fontSize:'50px' }}>♘</span>;
+	}
 }
